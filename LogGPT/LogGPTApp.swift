cf0f7e854caab8c3d7dcb39e876b5e91523a14a7
Logan Norman <logannormanss@gmail.com>
2023-02-20 00:29:20 -0800

    Initial Commit

---
+++ LogGPT/LogGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogGPTApp.swift
+//  LogGPT
+//
+//  Created by Logan Norman on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LogGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
